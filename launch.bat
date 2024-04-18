b6e379d61805d24ef031afe7f5e6a88bccd17356
gkscript <dominik.forer@netixx.it>
2024-04-18 12:46:01 +0200

    Add parameter and launch script directly

--- launch.bat
+++ launch.bat
@@ -2,5 +2,4 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 set dir="%CD%"
 powershell -Command "powershell.exe set-executionpolicy unrestricted"
-powershell -Command "powershell.exe Import-Module .\src\PSScriptMenuGui"
-powershell -Command "powershell.exe Show-ScriptMenuGui -csvpath .\src\gui.csv"
+powershell -Command "powershell.exe Import-Module .\src\PSScriptMenuGui; sleep 1; Show-ScriptMenuGui -noexit -csvpath .\src\gui.csv"
