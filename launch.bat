a109b0bc4cd183ace66f94d14ce10b61b3e86f01
gkscript <dominik.forer@netixx.it>
2024-04-18 14:49:29 +0200

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -2,4 +2,11 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 set dir="%CD%"
 powershell -Command "powershell.exe set-executionpolicy unrestricted"
-powershell -Command "powershell.exe Import-Module .\src\PSScriptMenuGui; sleep 1; Show-ScriptMenuGui -noexit -csvpath .\src\gui.csv"
+powershell -Command "Import-Module .\src\PSScriptMenuGui\PSScriptMenuGui.psm1; sleep 2; Show-ScriptMenuGui -csvpath .\src\gui.csv"
+
+
+
+
+
+
+
